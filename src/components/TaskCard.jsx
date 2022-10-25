fde7de1467d429880ce0e4cf4707b50e3e9663fe
joparedesm <joparedesm@unal.edu.co>
2022-10-25 08:48:06 -0500

    Ejemplo Contexto

--- src/components/TaskCard.jsx
+++ src/components/TaskCard.jsx
@@ -1,17 +1,17 @@
+import TaskContext from "../context/TaskContext";
+
 function TaskCard({task, deleteTask}) {
 
-  function mostrarAlerta(){
-    alert ('Eliminando...'+task.id)
-  }
-
   return (
-    <div>
-      <h1>{task.title}</h1>
-      <p>{task.description}</p>
-      <button onClick = { () => deleteTask(task.id) }>
-        Eliminar
-      </button>
-    </div>
+    <TaskContext>
+      <div>
+        <h1>{task.title}</h1>
+        <p>{task.description}</p>
+        <button onClick = { () => deleteTask(task.id) }>
+          Eliminar
+        </button>
+      </div>
+    </TaskContext>
   );
 }
 
---
+++ src/context/TaskContext.jsx
@@ -0,0 +1,10 @@
+function TaskContext({ children }) {
+  return (
+    <>
+    <h1>Componente Context</h1>
+    {children}
+    </>
+  )
+}
+
+export default TaskContext
